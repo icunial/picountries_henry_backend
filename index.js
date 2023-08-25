80758f78f7ae7d7b8830772020b8369c72c2dabf
Ignacio Cunial <igsecu@hotmail.com>
2023-08-25 16:13:45 -0300

    Added Res Headers -> index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Res Headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 // Initialized Express Server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
