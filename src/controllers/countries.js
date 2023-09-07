94621ac8f3fb4de4c78a7d87ec7320f9043c923d
Ignacio Cunial <igsecu@hotmail.com>
2023-09-07 16:20:23 -0300

    Created findCountryByIdApi() function controller -> get country by its id from API

--- src/controllers/countries.js
+++ src/controllers/countries.js
@@ -23,6 +23,36 @@ const getAllApi = async () => {
   }
 };
 
+// Get country by its id from API
+const findCountryByIdApi = async (id) => {
+  const result = [];
+
+  try {
+    const apiResults = await axios.get(API_URL);
+
+    if (apiResults) {
+      apiResults.data.forEach((r) => {
+        if (r.cca3 === id.toUpperCase()) {
+          result.push({
+            id: r.cc3,
+            name: r.name.common,
+            region: r.region,
+            flag: r.flags[1],
+            capital: r.capital[0],
+            subregion: r.subregion,
+            area: r.area,
+            population: r.population,
+          });
+        }
+      });
+    }
+    return result;
+  } catch (error) {
+    throw new Error("Error finding a country by its ID from API");
+  }
+};
+
 module.exports = {
   getAllApi,
+  findCountryByIdApi,
 };
