cd88c0d27c998cca2ccd7cc42b04897f90ec4601
Ignacio Cunial <igsecu@hotmail.com>
2023-09-07 16:25:30 -0300

    Updated GET / route -> added name query

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -28,7 +28,26 @@ router.get("/:id", async (req, res, next) => {
 
 // GET all countries
 router.get("/", async (req, res, next) => {
+  const { name } = req.query;
+
   try {
+    // Search country by its name
+    if (name) {
+      const apiResults = await countriesController.findByNameApi(name);
+
+      if (!apiResults.length) {
+        return res.status(404).json({
+          statusCode: 404,
+          msg: `Country with name ${name} not found`,
+        });
+      }
+
+      return res.status(200).json({
+        statusCode: 200,
+        data: apiResults,
+      });
+    }
+
     const apiResults = await countriesController.getAllApi();
 
     res.status(200).json({
