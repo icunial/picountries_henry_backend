d466f91f7f15391981a51fc9efb90661a307a570
Ignacio Cunial <igsecu@hotmail.com>
2023-09-07 17:47:40 -0300

    Created validateDifficulty() function

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -5,6 +5,17 @@ const validateName = (name) => {
   return false;
 };
 
+// validates difficulty
+const validateDifficulty = (difficulty) => {
+  if (!difficulty) return "Difficulty parameter is missing";
+  if (typeof difficulty !== "number") return "Difficulty must be a number!";
+  if (difficulty < 1 || difficulty > 5) {
+    return "Difficulty must be between 1 and 5";
+  }
+
+  return false;
+};
+
 module.exports = {
   validateName,
 };
