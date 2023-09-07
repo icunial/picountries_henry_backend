edf65f49b2dbdab1ec2c60300c65a801a33d0e41
Ignacio Cunial <igsecu@hotmail.com>
2023-09-07 17:56:29 -0300

    Updated POST / route -> added validations

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -6,8 +6,39 @@ const Activity = require("../models/Activity");
 const axios = require("axios");
 const countriesController = require("../controllers/countries");
 
+const validations = require("../utils/validations");
+
 router.post("/", async (req, res, next) => {
   const { name, difficulty, duration, season, countries } = req.body;
+
+  // Validations -> body parameters
+  if (validations.validateName(name)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateName(name),
+    });
+  }
+
+  if (validations.validateDifficulty(difficulty)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateDifficulty(difficulty),
+    });
+  }
+
+  if (validations.validateDuration(duration)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateDuration(duration),
+    });
+  }
+
+  if (validations.validateSeason(season)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validations.validateSeason(season),
+    });
+  }
 });
 
 module.exports = router;
