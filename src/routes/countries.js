7501cec54ce66269c8bd7e953dd71a25980f4894
Ignacio Cunial <igsecu@hotmail.com>
2023-09-07 16:13:38 -0300

    Created GET /:id route -> get country by its id

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -3,6 +3,29 @@ const router = express.Router();
 
 const countriesController = require("../controllers/countries");
 
+// Get country by its id
+router.get("/:id", async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const apiResults = await countriesController.findCountryByIdApi(id);
+
+    if (!apiResults.length) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Country with ID: ${id} not found!`,
+      });
+    }
+
+    res.status(200).json({
+      statusCode: 200,
+      data: apiResults,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
 // GET all countries
 router.get("/", async (req, res, next) => {
   try {
