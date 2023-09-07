59b4d95d21381b5e95d76172303e61d7293344fc
Ignacio Cunial <igsecu@hotmail.com>
2023-09-07 16:01:34 -0300

    Created GET /countries route -> get all countries from API

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -3,4 +3,18 @@ const router = express.Router();
 
 const countriesController = require("../controllers/countries");
 
+// GET all countries
+router.get("/", async (req, res, next) => {
+  try {
+    const apiResults = await countriesController.getAllApi();
+
+    res.status(200).json({
+      statusCode: 200,
+      data: apiResults,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
 module.exports = router;
