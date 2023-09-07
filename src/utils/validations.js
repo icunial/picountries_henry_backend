90fbd5ae8de019315582123d97c84f4c285b5a25
Ignacio Cunial <igsecu@hotmail.com>
2023-09-07 18:06:56 -0300

    Updated validateSeason() function -> changed logical operators

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -28,9 +28,9 @@ const validateSeason = (season) => {
   if (!season) return "Season parameter is missing";
   if (typeof season !== "string") return "Season must be a string!";
   if (
-    season.toLowerCase() !== "autumn" ||
-    season.toLowerCase() !== "winter" ||
-    season.toLowerCase() !== "spring" ||
+    season.toLowerCase() !== "autumn" &&
+    season.toLowerCase() !== "winter" &&
+    season.toLowerCase() !== "spring" &&
     season.toLowerCase() !== "summer"
   ) {
     return "Season must be AUTUMN, WINTER, SPRING or SUMMER";
